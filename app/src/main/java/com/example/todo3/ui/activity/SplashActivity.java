66de2b4cd5043ba37546cf1b24d8aea20e6c9d0c
shwetanknaveen <shwetanknaveen@gmail.com>
2019-09-17 16:35:05 +0530

    splash screen files

---
+++ app/src/main/java/com/example/todo3/ui/activity/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.todo3.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.example.todo3.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        if(getSupportActionBar()!=null)
+            getSupportActionBar().hide();
+        ImageView imageView = findViewById(R.id.splashScreenImageView);
+        imageView.setBackgroundResource(R.drawable.todo_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },SPLASH_TIMEOUT);
+    }
+}
